aedf534f19e7ed5be5eaf2b2916d401e930fb928
IanZGray <ianzacharygray@gmail.com>
2023-08-02 11:04:35 -0400

    added logo

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './Hero'
+import Testimonials from './Testimonials'
+import About from './About'
+
+function Main() {
+  return (
+    <>
+    <Hero/>
+    <Testimonials/>
+    <About/>
+    </>
+  )
+}
+
+export default Main
