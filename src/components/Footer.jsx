ff491d5b1c21088df6bc147a63e6dbb4c47457d9
IanZGray <ianzacharygray@gmail.com>
2023-08-02 11:57:53 -0400

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import logo from '../images/logo-vertical-white.png'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer' >
+        <img src={logo} alt='little lemon logo' />
+        <ul className='footerList' >
+            <li><h4>Doormat Navigation</h4></li>
+            <li><p>Home</p></li>
+            <li><p>About</p></li>
+            <li><p>Menu</p></li>
+            <li><p>Reservations</p></li>
+            <li><p>Order Online</p></li>
+            <li><p>Login</p></li>
+        </ul>
+        <ul className='footerList' >
+            <li><h4>Contact</h4></li>
+            <li><p>Address</p></li>
+            <li><p>Phone Number</p></li>
+            <li><p>Email</p></li>
+        </ul>
+        <ul className='footerList' >
+            <li><h4>Social Media Links</h4></li>
+            <li><p>Address</p></li>
+            <li><p>Phone Number</p></li>
+            <li><p>Email</p></li>
+        </ul>
+    </div>
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import logo from '../images/logo-original.png'
 
 function Nav() {
   return (
     <nav>
-        <img src='./images/logo-original.png' alt='little lemon logo' />
+        <img className='logo' src={logo} alt='little lemon logo' />
         <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Menu</li>
-            <li>Reservations</li>
-            <li>Order Online</li>
-            <li>Login</li>
+            <li ><a href='/'>Home</a></li>
+            <li><a href='/About'>About</a></li>
+            <li><a href='/Menu'>Menu</a></li>
+            <li><a href='/Reservations'>Reservations</a></li>
+            <li><a href='/Order Online'>Order Online</a></li>
+            <li><a href='/Login'>Login</a></li>
         </ul>
     </nav>
   )
