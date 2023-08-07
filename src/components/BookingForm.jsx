fb46939bbd621d6520c47b4a16d50ba970bca615
IanZGray <ianzacharygray@gmail.com>
2023-08-07 14:57:17 -0400

    pre build

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -4,9 +4,14 @@ import { React, useEffect, useState } from 'react'
 function BookingForm({ selections, selectionFunc, times, timesDispatch }) {
     let timesMap = times || []
 
+    // const [dateError, setDateError] = useState('')
+    // const [timeError, setTimeError] = useState('')
+    // const [guestsError, setGuestsError] = useState('')
+    // const [occasionError, setOccasionError] = useState('')
+
     const handleSubmit = (e) => {
         e.preventDefault();
-        alert(`Thank you for booking your meal on ${selections.date} at ${selections.time} o'Clock for ${selections.gests} guests. We hope you enjoy your ${selections.occassion}`)
+        alert(`Thank you for booking your meal on ${selections.date} at ${selections.time} o'Clock for ${selections.gests} guests. We hope you enjoy your ${selections.occasion}`)
     }
 
     const handleChange = (e) => {
@@ -15,38 +20,44 @@ function BookingForm({ selections, selectionFunc, times, timesDispatch }) {
     }
 
   return (
-    <form >
+    <form onSubmit={handleSubmit}>
         <h1
-            style={{'margin-bottom': '20px'}}
+            style={{
+                'margin-bottom': '20px'
+            }}
         >Reservations</h1>
-        <label htmlFor="res-date">Choose Date</label>
+
+        <label htmlFor="res-date">Choose Date*</label>
         <input type="date" id="res-date" data-testid='calendar' required onChange={handleChange}/>
-        <label htmlFor="res-time">Choose Time</label>
+        {/* <p>{dateError}</p> */}
+
+        <label htmlFor="res-time">Choose Time*</label>
         <select id="res-time" data-testid='time-pulldown' required onChange={(e)=> selectionFunc({...selections, time : e.target.value})} >
             {timesMap.map((time) => {
                 return <option key={time} data-testid='times' >{time}</option>
             })}
         </select>
+        {/* <p>{timeError}</p> */}
 
-        <label htmlFor="guests">Number of Guests</label>
+        <label htmlFor="guests">Number of Guests*</label>
         <input type="number" placeholder="How Many?" min="1" max="10" id="guests" required onChange={(e)=> selectionFunc({...selections, guests : e.target.value})}/>
+        {/* <p>{guestsError}</p> */}
+
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion" min='1' onChange={(e)=> selectionFunc({...selections, occassion : e.target.value})}>
+        <select id="occasion" min='1' onChange={(e)=> selectionFunc({...selections, occasion : e.target.value})}>
             <option>Birthday</option>
             <option>Anniversary</option>
         </select>
+        {/* <p>{occasionError}</p> */}
+
         <button
             type="submit"
             style={{
                 'alignSelf': 'center',
                 'width': '55%',
+                'marginTop': '20px',
             }}
-            onClick={(e) => {handleSubmit(e)}}
         >Book Now</button>
-        {/* <h2 style={{'color':'black'}}>{selections.date || 'hi'}</h2>
-        <h2 style={{'color':'black'}}>{selections.time || 'hi'}</h2>
-        <h2 style={{'color':'black'}}>{selections.guests || 'hi'}</h2>
-        <h2 style={{'color':'black'}}>{selections.occassion || 'hi'}</h2> */}
     </form>
   )
 }
